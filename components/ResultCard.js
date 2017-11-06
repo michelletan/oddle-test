7fd93106de3121dd76574305860b68ea9ef79e86
Michelle Tan <michelletanjm@gmail.com>
2017-11-06 17:39:32 +0800

    Update ResultCard

--- components/ResultCard.js
+++ components/ResultCard.js
@@ -2,22 +2,39 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const Card = styled.div`
-  flex: 1;
-  flex-direction: column;
+  display: flex;
+  flex-direction: row;
   justify-content: stretch;
   padding: 10px;
 `
 
-const CardImage = styled.img`
-  width: 100px;
-  height: 100px;
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+`
+
+const Content = styled.div`
+  margin-left: 10px;
+`
+
+const Text = styled.div`
+  margin: 0;
+`
+
+const Title = styled.div`
+  flex: 1;
+  font-size: 18px;
 `
 
 export class ResultCard extends React.Component {
   render() {
     return (
       <Card>
-        <CardImage src="http://i0.kym-cdn.com/photos/images/original/001/250/216/305.jpg"/>
+        <Image src="http://i0.kym-cdn.com/photos/images/original/001/250/216/305.jpg"/>
+        <Content>
+          <Title>Card Title</Title>
+          <Text>Following Followers</Text>
+        </Content>
       </Card>
     )
   }
--- components/SearchBar.js
+++ components/SearchBar.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  flex: 1;
+  display: flex;
   flex-direction: column;
+  align-items: stretch;
   padding: 10px;
 `
 const Input = styled.input`
-  width: 500px;
   margin: 0px;
   padding: 7px;
 `
