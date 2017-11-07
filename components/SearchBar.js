ebbe10d10b0e8bf4084a06b4fd54857c60776534
Michelle Tan <michelletanjm@gmail.com>
2017-11-07 12:14:56 +0800

    Add button for search bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -3,21 +3,26 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: stretch;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
   padding: 10px;
 `
 const Input = styled.input`
+  flex: 1;
   margin: 0px;
   padding: 7px;
 `
-
+const Button = styled.button`
+  width: 100px;
+  padding: 9px;
+`
 
 export class SearchBar extends React.Component {
   render() {
     return (
       <Container>
-        <Input type="text" placeholder="Search for a user on GitHub..."/>
+        <Input type="text" placeholder="Search for a user on GitHub..."/><Button>Search</Button>
       </Container>
     )
   }
