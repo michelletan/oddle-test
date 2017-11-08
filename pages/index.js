a49169227e0de75f0dfeb0616367825489cf8280
Michelle Tan <michelletanjm@gmail.com>
2017-11-08 14:25:22 +0800

    Add wrapper for next-redux

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,13 @@ import Router from 'next/router'
 import styled from 'styled-components'
 import { injectGlobal } from 'styled-components'
 
+import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
+import withRedux from 'next-redux-wrapper'
+import rootReducer from '../reducers'
+
+import { fetchSearchResults } from '../actions'
+
 import Header from '../components/Header'
 import SearchBar from '../components/SearchBar'
 import Pagination from '../components/Pagination'
@@ -24,19 +31,52 @@ const Container = styled.div`
   align-items: stretch;
 `
 
-export default class App extends React.Component {
-  static async getInitialProps({ req }) {
-    return { foo: 'foo' }
+const makeStore = (initialState, options) => {
+  return createStore(rootReducer, initialState, compose(applyMiddleware(thunkMiddleware)))
+}
+
+export class App extends React.Component {
+  static async getInitialProps({ store, isServer, pathname, query }) {
+    return { }
   }
 
   render() {
     return (
       <Container>
         <Header />
-        <SearchBar />
-        <Pagination />
-        <ResultList />
+        <SearchBar onSearchRequest={this.props.onSearchRequest}/>
+        <Pagination resultRange={this.props.resultRange}/>
+        <ResultList users={this.props.users}/>
       </Container>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  let users = []
+  let resultRange = { total: 0, start: 0, end: 0 }
+
+  if (state.usersByQueries[state.selectedQuery]) {
+    const results = state.usersByQueries[state.selectedQuery]
+    users = results['items']
+    resultRange.total = results['total_count']
+    resultRange.start = 1
+    resultRange.end = 30
+  }
+  return {
+    users: users,
+    resultRange: resultRange
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchRequest: query => {
+      dispatch(fetchSearchResults(query))
+    }
+  }
+}
+
+App = withRedux(makeStore, mapStateToProps, mapDispatchToProps)(App)
+
+export default App
